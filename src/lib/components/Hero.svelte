8e9159e6c9360178a183419125fc64ae624ce9b2
Monstroe <colemontrose@gmail.com>
2026-01-12 22:54:51 -0500

    Latest

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,51 +1,47 @@
-<section>
-    <div class="background">
-        <div class="el"></div>
+<script lang="ts">
+	import { inView } from '$lib/actions/animations';
+	import Spacer from './Spacer.svelte';
+</script>
+
+<section class="background">
+    <div class="el">
+        <div class="text">
+            <h1 use:inView class="animate from-top">The Possibilities are Endless and We're Here to Create</h1>
+            <Spacer width="80%" color="var(--color-background-primary)" />
+            <h2 use:inView class="animate from-bottom">DeMo Technology</h2>
+        </div>
     </div>
 </section>
 
 <style>
+    .text {
+        position: relative;
+		z-index: 1;
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+        padding: 0 1rem;
+    }
 
-    .hero {
+	.text h1 {
+		text-align: center;
 		position: relative;
-		height: 100vh;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		overflow: hidden;
+		font-size: clamp(3rem, 12vw, 6rem);
+		font-weight: 500;
+		color: var(--color-background-primary);
+		text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
 	}
 
-	.banner {
-		height: 80vh;
-		overflow: hidden;
+	.text h2 {
+		text-align: center;
+		position: relative;
+		font-size: clamp(1.5rem, 4vw, 3rem);
+		font-weight: 300;
+		color: var(--color-background-primary);
+		text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
 	}
 
-	.banner img {
-		object-fit: cover;
-		object-position: center;
-		filter: blur(4px);
-		width: 100%;
-		height: 100%;
-	}
-
-    h1 {
-		position: fixed;
-		top: 50%;
-		left: 50%;
-		translate: -50% -50%;
-		margin: 0;
-		font-size: clamp(6rem, 8vw + 1rem, 14rem);
-		font-family: 'Geist', sans-serif;
-		font-weight: 140;
-		color: hsl(0 0% 2%);
-		mix-blend-mode: soft-light;
-		/*   -webkit-text-stroke: 2px hsl(0 0% 100% / 0.95); */
-		filter: drop-shadow(0 0 2px white);
-		text-shadow: 2px 2px white;
-	}
-
-    	* {
+	* {
 		box-sizing: border-box;
 	}
 
@@ -57,27 +53,39 @@
 		display: grid;
 		place-items: center;
 		min-height: 100vh;
-		background: hsl(0 0% 6%);
+        background-color: var(--color-text-primary);
 	}
 
 	.el {
+		position: relative;
+		display: grid;
+		place-items: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.el::before {
+		content: '';
+		position: absolute;
+		inset: 0;
 		background: conic-gradient(
 			from 180deg at 50% 70%,
 			hsla(0, 0%, 98%, 1) 0deg,
-			#eec32d 72.0000010728836deg,
-			#ec4b4b 144.0000021457672deg,
-			#709ab9 216.00000858306885deg,
-			#4dffbf 288.0000042915344deg,
+			#eec32d 72deg,
+			#ec4b4b 144deg,
+			#709ab9 216deg,
+			#4dffbf 288deg,
 			hsla(0, 0%, 98%, 1) 1turn
 		);
-		width: 100%;
-		height: 100%;
+
 		mask:
 			radial-gradient(circle at 50% 50%, white 2px, transparent 2.5px) 50% 50% / var(--size)
 				var(--size),
 			url('https://assets.codepen.io/605876/noise-mask.png') 256px 50% / 256px 256px;
+
 		mask-composite: intersect;
-		animation: flicker 20s infinite linear;
+		animation: flicker 20s linear infinite;
+		z-index: 0;
 	}
 
 	@keyframes flicker {
