c0df64270237776da7e2c2f655236a2acf239ee5
j-demartini <josephdemartini801@gmail.com>
2026-01-12 21:11:06 -0500

    dwa

--- src/lib/components/sections/about/SectionAbout.svelte
+++ src/lib/components/sections/about/SectionAbout.svelte
@@ -5,7 +5,7 @@
 <div class="section">
 	<section id="about" class="about">
 		<div class="about-text">
-			<h1 use:inView class="section-text-title animate from-top">2 guys with a crazy idea.</h1>
+			<h1 use:inView class="section-text-title animate from-top">Welcome to <font color="black">DeMo Technology</font></h1>
 			<p use:inView class="section-text-content animate from-left">
 				DeMo Technology, based in the greater Orlando area, bridges the gap between software and hardware. Coming from diverse backgrounds in software, simulation, themed entertainment, and electronics, our multidisciplinary team specializes
 				in putting all of these together to create high-fidelity experiences supported by reliable backend systems.
@@ -14,7 +14,7 @@
 		<img
 			use:inView
 			class="about-img glow-sm animate from-right"
-			src="/images/about/profile.jpg"
+			src="/images/banners/colejosephbanner.jpg"
 			alt="Profile"
 		/>
 	</section>
@@ -27,11 +27,13 @@
 
 	img {
 		border-color: var(--color-background-alternate);
-		border-radius: 2048px;
+		border-radius: 0vw;
 		border-width: 0.3rem;
 		border-style: solid;
-		max-width: 20rem;
-		max-height: 20rem;
+		max-width: 25rem;
+		max-height: 25rem;
+
+		background-size:cover;
 	}
 
 	.section {
@@ -57,5 +59,10 @@
 
 	.about-text {
 		max-width: 52rem;
+
+		display:flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items:flex-start;
 	}
 </style>
